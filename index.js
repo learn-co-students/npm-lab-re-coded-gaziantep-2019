988a4f677da95c6c14097ab2500533c0cd2d6409
kassem241 <34236354+kassem241@users.noreply.github.com>
2019-07-26 14:11:56 +0300

    update js

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function displayTime() {
 }
 
 window.onload = displayTime()
+
+function displayTime() {
+ let clock = document.querySelector('#clock')
+ clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
+ setInterval(function() {
+     clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
+     clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+     clock.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+ }, 1000)
+}
+window.onload = displayTime()
+
